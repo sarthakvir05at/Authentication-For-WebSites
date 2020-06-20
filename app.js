a66073b1aec9d1c1bbee39646bae59910e244829
Sarthak Jaiswal <sarthakjaiswal15@gmail.com>
2020-06-20 23:58:25 +0530

    Level 3, Hasing a password

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express= require('express');
 const bodyParser= require('body-parser');
 const ejs= require('ejs');
 const mongoose= require('mongoose');
-const encrypt= require('mongoose-encryption');
+const md5= require('md5');
 
 const app= express();
 
@@ -20,7 +20,7 @@ const userSchema= new mongoose.Schema({
 
 // Adding auth by encrypting our password with a key of secret which can be any string
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User= new mongoose.model("User", userSchema);
 
@@ -40,7 +40,7 @@ app.post("/register", (req,res) => {
 
     const newUser= new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     newUser.save((err) => {
         if(err)
@@ -53,7 +53,7 @@ app.post("/register", (req,res) => {
 app.post("/login", (req,res) => {
 
     const username= req.body.username;
-    const password= req.body.password;
+    const password= md5(req.body.password);
 
     User.findOne({ email: username }, (err,doc) => {
         if(!doc)
