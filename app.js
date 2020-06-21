5e4394abc3706558f5db0502e561637cd8fe7a78
Sarthak Jaiswal <sarthakjaiswal15@gmail.com>
2020-06-21 23:52:49 +0530

    Level 6, OAuth2.0 & Implement Sign In With Google

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const mongoose= require('mongoose');
 const session = require('express-session');
 const passport= require('passport');
 const passportLocalMongoose= require('passport-local-mongoose');
-const e = require('express');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate= require('mongoose-findorcreate');
 
 const app= express();
 
@@ -29,10 +30,12 @@ mongoose.set("useCreateIndex", true);       // Depreciation Warning
 
 const userSchema= new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String
 });
 
 userSchema.plugin(passportLocalMongoose); // using passportLocalMongoose as a plugin for userSchema
+userSchema.plugin(findOrCreate);
 
 // Adding auth by encrypting our password with a key of secret which can be any string
 
@@ -43,12 +46,45 @@ const User= new mongoose.model("User", userSchema);
 passport.use(User.createStrategy());  // create local login strategy using passport
 
 //setting passport to serialize and deserialize User
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
+// Using oAuth And configuring with the google oauth
+// Also findOrCreate want a method before so mongoose converted it to method and need to install mongoose-findOrCreate
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+      console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
 
 app.get("/", (req,res) => {
     res.render("home");
 })
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));
+
+app.get('/auth/google/secrets', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  });
 
 app.get("/login", (req,res) => {
     res.render("login");
